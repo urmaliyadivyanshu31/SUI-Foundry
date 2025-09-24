c7c6fa1b7f42dae7516c1fe64bf08372073d3337
urmaliyadivyanshu31 <urmaliyadiv04@gmail.com>
2025-09-24 21:33:10 +0530

    Fix textarea interface TypeScript error for deployment

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -3,7 +3,9 @@ import * as React from "react"
 import { cn } from "@/lib/core/utils"
 
 export interface TextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+  // Extending React.TextareaHTMLAttributes for shadcn/ui component API
+}
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, ...props }, ref) => {
